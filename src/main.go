47a1285aad8a18d9717748db6f23520b6678671c
Harris Schrick <schrickh+github@gmail.com>
2023-09-13 21:59:43 -0700

    init basic frontend routing

--- src/main.go
+++ src/main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+	router := gin.Default()
+
+	// Serve frontend static files
+	router.Use(static.Serve("/", static.LocalFile("./views", true)))
+
+	//Setup route group for the API
+	api := router.Group("/api")
+	{
+		api.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
 		})
-	})
-	r.Run() // 8080/ping
+	}
+
+	router.Run(":4000") // 8080/ping
 }
